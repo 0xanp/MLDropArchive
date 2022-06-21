638bb2ac231149b08c8ab45af729df7187a0423a
Binh An <anbinhpham110799@gmail.com>
2022-06-21 11:14:22 -0400

    cleanup before branching

--- pages/1_🔔_Curated_Follows.py
+++ pages/1_🔔_Curated_Follows.py
@@ -1,8 +1,22 @@
 import streamlit as st
-
+from twitter_auth import bearer_token
+import tweepy
 st.set_page_config(page_title="Curated Follows Feed", page_icon="🔔", layout="wide")
 st.markdown("# Curated Follows Feed Demo")
 st.sidebar.header("Some filtering")
 st.write(
     """This is placeholder page for the curated follows feed"""
 )
+# ----- ESTABLISH A TWEEPY CLIENT 
+client = tweepy.Client(bearer_token=bearer_token)
+# Curated list of usernames we want to track
+curated_lists = ['0xAnP','itsjvon']
+
+database = {}
+users = client.get_users(ids=None, usernames=curated_lists)
+ids = [data.id for data in users.data]
+for id in ids:
+    for response in tweepy.Paginator(client.get_users_following, id,
+                                   max_results=1000):
+        database[id] = response
+print(database[583415381].data)
